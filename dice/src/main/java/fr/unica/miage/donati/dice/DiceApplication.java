ee6e0c209c06e328846d24ed367d9c889077668b
Leo Donati <leo.donati@unice.fr>
2024-12-13 11:51:09 +0100

    Add RestController

--- dice/src/main/java/fr/unica/miage/donati/dice/DiceApplication.java
+++ dice/src/main/java/fr/unica/miage/donati/dice/DiceApplication.java
@@ -2,9 +2,17 @@ package fr.unica.miage.donati.dice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class DiceApplication {
+
+	@RequestMapping("/")
+	public String home() {
+		return "Hello Miage groupe TD1";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(DiceApplication.class, args);
